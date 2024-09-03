5bb1053c9f3e78d3bf670e811aa56206aacba718
Will <Will@MacWill.local>
2024-09-03 22:56:52 +0200

    création d'un profil en python

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
-print ("hello world!")
+nom = "Snow"
+prenom = "Jon"
+taille = 1.80
+Is_student = False
+
+print (f"Salut, moi c'est {prenom} {nom} je mesure {taille} metres, étudiant ? {Is_student}")
+
+type(nom)
+type(prenom)
+type(taille)
+type(Is_student)

452c26fcd52ccd9a2b4bc9fcc91f4b622bf07af7
Will <Will@MacWill.local>
2024-09-08 13:27:27 +0200

    ajout des dictionnaire dans le 1ercode python

--- helloworld.py
+++ helloworld.py
@@ -33,4 +33,43 @@ fruits.reverse()
 print (fruits)
 
 fruits.pop()
+Dico={"loup_blanc":"jon snow","une_fille":"arya","chien":"clegane"}
 
+Portrait_philo_jon = {
+    "nom":"Jon Snow", "philosophie":"le devoir", "devise":"l'amour eest la mort du devoir",
+    "Allégeance":("Stark","targaryen","baratheon"),
+    "Siblings":["Robb","Bran","Arya","Sansa","Neth"],
+    "Titre" : "Roi de l'hiver"
+}
+
+physique_jon = {}
+physique_jon["yeux"] = "noir"
+physique_jon["taille"]= 1.80
+physique_jon["cheveux"] = "noir"
+
+physique_jon.keys()
+physique_jon.values()
+print(physique_jon)
+
+Portrait_philo_jon["origine"] = "first men"
+Portrait_philo_jon.items()
+
+Portrait_philo_jon.pop("origine")
+print(Portrait_philo_jon)
+
+"taille" in physique_jon
+
+
+fruits={
+    "orange":"orange", "pomme":"rouge","banane":"jaune"
+}
+
+fruits["kiwi"] = "vert"
+
+couleur_banane = fruits["banane"]
+
+fruits["pomme"] = "vert"
+
+fruits.pop["banane"]
+
+fruits.items()

e8f1f22626bdee7629c3c3b01079a6789e4e2cb1
Will <Will@MacWill.local>
2024-09-24 23:40:38 +0200

    division par zero

--- conditions.py
+++ conditions.py
@@ -1,1 +1,68 @@
 print ('hello, world ! bitch ass')
+
+ensoleille = True
+on_en_semaine = True
+on_est_en_weekend = False
+
+if ensoleille and not on_est_en_weekend:
+    print("pas de plage demain je taff")
+elif ensoleille and on_est_en_weekend:
+    print("on va à la plage !")
+else:
+    print("on reste à la maison !")
+
+
+nombre_de_sieges = 30
+nombre_dinvites = 25
+
+if nombre_dinvites < nombre_de_sieges:
+    print("plus d'invité")
+else:
+    print("la salle est pleine")
+
+
+
+fruit = "_"
+match fruit:
+    case "pomme":
+        print("J'aime les pommes !")
+    case "banane":
+        print("Je n'aime pas les bananes.")
+    case "orange":
+        print("Les oranges sont bonnes pour la santé.")
+    case _:
+        print("Je ne connais pas ce fruit.")
+
+
+nombre_a_gauche = 23
+nombre_a_droite = 8
+
+symbole = "*"
+
+resultat = 0
+
+if not isinstance(nombre_a_gauche,int) or not isinstance (nombre_a_droite,int) :
+    print ("erreur les variables ne sont pas des entiers")
+    # isinstance me permet de vérifier le type de mon objet
+
+match symbole :
+    case "+" :
+        print("symbole plus")
+    case "-" :
+        print("symbole moins")
+    case "*" :
+        print("Bien joué, symbole multiplication")
+    case "/" :
+        print ("symbole division")
+    case _:
+        print("symbole inexistant")    
+
+
+match symbole : 
+    case "/":
+        if nombre_a_droite == 0:
+            print ("impossible de diviser par 0")
+        else:
+            resultat = nombre_a_gauche/nombre_a_droite
+            print (resultat)
+

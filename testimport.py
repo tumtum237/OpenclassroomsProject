323d7120dbd42f9a465cd89af46e2421f3db6f8e
Will <Will@MacWill.local>
2024-10-17 13:27:39 +0200

    ajout création ETL en CSV

--- testimport.py
+++ testimport.py
@@ -1,8 +1,23 @@
-from operations import addition
+#from operations import addition
 
-addition (40,10)
+#addition (40,10)
 
 
-import operations
+#import operations
 
-operations.multiplication(4,5)
+#operations.multiplication(4,5)
+
+import csv
+
+with open('input.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile) 
+    for row in reader:
+        nom = row[0]
+        heures_travaillees = row[1]
+        print(f"{nom} a travaillé {heures_travaillees} heures.")
+
+en_tete = ("Nom", "Salaires")
+with open ('output.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, delimiter= ',')
+    writer.writerow(en_tete)
+    
